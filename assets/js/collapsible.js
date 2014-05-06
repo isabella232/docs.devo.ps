71dd8bc5ce5e14e83ff51d7cd2aa38dfd734c141
Ronan Berder <ronan@wiredcraft.com>
2014-05-06 19:10:53 +0800

    Handling more nicely menu and small width

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
+	// Make nested lists collapsible
 	$('#sidebar h3').click(function () {
 		var container = $(this).parent();
 		if (container.hasClass('active')) container.removeClass('active');
 		else { container.addClass('active'); }
 	});
+
+    // Expand link to current page
+	var pathname = window.location.pathname;
+	console.log();
 });
