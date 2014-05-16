ff136bf359e1692163dc5f233bab9ed4f0eb6678
Vincent Viallet <vincent@wiredcraft.com>
2014-05-16 11:38:51 +0800

    Cleanup

--- Makefile
+++ Makefile
@@ -1,15 +1,11 @@
-dev: install  bower
+dev: install
 	@./node_modules/.bin/gulp development --open --port 4000
 
 install:
-	# @npm --registry=http://r.cnpmjs.org \
-       #--cache=${HOME}/.npm/.cache/cnpm \
-       #--disturl=http://cnpmjs.org/dist \
-       #--userconfig=${HOME}/.cnpmrc \
-       #install
 	@npm install
-
-bower: install
 	@./node_modules/.bin/bower install
 
-.PHONY: dev install bower
+build: install
+	@./node_modules/.bin/gulp
+
+.PHONY: dev install build
--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 # Meant to be called by devops builder
 ###################
 
+export HERE=$(pwd)
+
 COMMIT_MSG=$1
 if [ -z "$COMMIT_MSG" ]; then
     # default commit message
@@ -11,30 +13,63 @@ if [ -z "$COMMIT_MSG" ]; then
 fi
 
 TMP_FOLDER=$(mktemp -d)
+TMP_FOLDER2=$(mktemp -d)
+
+cd $TMP_FOLDER
 
 #
-# Build process
+# Fetch wiki
 #
-npm --silent install
-bower --silent install
-gulp
+echo "Fetching docs wiki"
+git clone git@github.com:devo-ps/docs.devo.ps.wiki.git
+echo "Fetching services details"
+git clone git@github.com:devo-ps/devops-ansible.git
+
+#
+# Copy wiki to manual
+#
+cd docs.devo.ps.wiki
+mkdir -p $HERE/source/manual
+cp * $HERE/source/manual
+
+#
+# Copy services files to references
+#
+cd ../devops-ansible
+# copy 
+function copy() {
+    file="$1"
+    dir=$(dirname $file)
+    base=$(basename $file \.yml)
+
+    newname=$(echo $dir | cut -f2 -d'/')
+
+    cp $file $HERE/source/references/$newname.md
+}
+export -f copy
+
+find packages -name configuration.md -exec bash -i -c 'copy {}' \;
+
+#
+# Prepare the menu data file
+#
+cd $HERE
+python build/prepare_menu.py
+make build
 
 # Copy build files
-cp -a _site/* $TMP_FOLDER
-
-#
-# Deployment
-#
+cp -a _site/* $TMP_FOLDER2
 
 # Stash changes to allow branch switch
 git stash
 git checkout gh-pages
 git clean -f -d
 git clean -f -x
-cp -a $TMP_FOLDER/* .
+cp -a $TMP_FOLDER2/* .
 git add .
 git commit -am "$COMMIT_MSG"
 git push
 
 # Cleanup
 rm -rf $TMP_FOLDER
+rm -rf $TMP_FOLDER2
