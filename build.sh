194f01aca0f8d5fe22382b742b60d3d68b423234
Vincent Viallet <vincent@wiredcraft.com>
2014-06-20 14:29:39 +0800

    Update build process to simply build locally and push to gh-pages, all sources are already present

--- build.sh
+++ build.sh
@@ -13,17 +13,17 @@ if [ -z "$COMMIT_MSG" ]; then
 fi
 
 TMP_FOLDER=$(mktemp -d)
-TMP_FOLDER2=$(mktemp -d)
+# TMP_FOLDER2=$(mktemp -d)
 
-cd $TMP_FOLDER
-echo "Fetching services / providers details"
-git clone --branch docs git@github.com:devo-ps/devops-ansible.git
+# cd $TMP_FOLDER
+# echo "Fetching services / providers details"
+# git clone --branch docs git@github.com:devo-ps/devops-ansible.git
 
-#
-# Copy wiki to manual
-#
-cd devops-ansible
-cp -a * $HERE/source/
+# #
+# # Copy wiki to manual
+# #
+# cd devops-ansible
+# cp -a * $HERE/source/
 
 #
 # Prepare the menu data file
@@ -34,7 +34,7 @@ mkdir public
 make build
 
 # Copy build files
-cp -a _site/* $TMP_FOLDER2
+cp -a _site/* $TMP_FOLDER
 
 # Stash changes to allow branch switch
 git stash
@@ -42,11 +42,11 @@ git checkout gh-pages
 git clean -f -d
 git clean -f -x
 git pull
-cp -a $TMP_FOLDER2/* .
+cp -a $TMP_FOLDER/* .
 git add .
 git commit -am "$COMMIT_MSG"
 git push
 
 # Cleanup
 rm -rf $TMP_FOLDER
-rm -rf $TMP_FOLDER2
+# rm -rf $TMP_FOLDER2
