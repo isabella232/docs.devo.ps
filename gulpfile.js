6e1d82889abba77e4620da23341541c69d2aa139
Ronan Berder <hunvreus@gmail.com>
2014-08-29 13:05:23 +0800

    Cleaning up the provider theme

--- gulpfile.js
+++ gulpfile.js
@@ -62,6 +62,11 @@ gulp.task('fonts', function() {
         .pipe(gulp.dest('./public/assets/fonts'));
 })
 
+gulp.task('images', function() {
+    gulp.src(site.assets.custom.images)
+        .pipe(gulp.dest('./public/assets/images'));
+})
+
 //
 gulp.task('metalsmith', function(callback) {
     var metalsmith = new Metalsmith(process.cwd());
